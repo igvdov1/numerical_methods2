b2120b0d45ebac0f1e6db8022ff77efe32edd2ba
igvdov1 <i.vdovin1@g.nsu.ru>
2023-09-19 18:18:59 +0700

    second lab

---
+++ lab2/newton_meth.py
@@ -0,0 +1,16 @@
+import numpy as np
+
+def newton_method(x0, f,derivative, eps=1e-8):
+    x2 = x0 + 1
+    count = 0
+
+
+    while (abs(x0 - x2) > eps):
+        x1 = x0 - f(x0) / derivative(x0)
+        x2 = x0
+        x0 = x1
+        count += 1
+
+
+
+    return np.round(x2, 6), count
